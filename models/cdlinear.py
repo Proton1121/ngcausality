0c925770cd6a79ce242ee8bfdc126b2d63780d83
Proton1121 <71938858+Proton1121@users.noreply.github.com>
2024-11-10 17:10:35 +0100

    Update cdlinear.py

--- models/cdlinear.py
+++ models/cdlinear.py
@@ -35,53 +35,50 @@ class series_decomp(nn.Module):
         res = x - moving_mean
         return res, moving_mean
 
-class Model(nn.Module):
+class DLinear(nn.Module):
     """
     Decomposition-Linear
     """
-    def __init__(self, configs):
-        super(Model, self).__init__()
-        self.seq_len = configs.seq_len
-        self.pred_len = configs.pred_len
+    def __init__(self, num_series, hidden, lag, kernel_size):
+        super(DLinear, self).__init__()
 
         # Decompsition Kernel Size
-        kernel_size = 25
         self.decompsition = series_decomp(kernel_size)
-        self.individual = configs.individual
-        self.channels = configs.enc_in
 
-        if self.individual:
-            self.Linear_Seasonal = nn.ModuleList()
-            self.Linear_Trend = nn.ModuleList()
-            
-            for i in range(self.channels):
-                self.Linear_Seasonal.append(nn.Linear(self.seq_len,self.pred_len))
-                self.Linear_Trend.append(nn.Linear(self.seq_len,self.pred_len))
-
-                # Use this two lines if you want to visualize the weights
-                # self.Linear_Seasonal[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-                # self.Linear_Trend[i].weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-        else:
-            self.Linear_Seasonal = nn.Linear(self.seq_len,self.pred_len)
-            self.Linear_Trend = nn.Linear(self.seq_len,self.pred_len)
-            
-            # Use this two lines if you want to visualize the weights
-            # self.Linear_Seasonal.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
-            # self.Linear_Trend.weight = nn.Parameter((1/self.seq_len)*torch.ones([self.pred_len,self.seq_len]))
+        self.Linear_Seasonal = nn.Conv1d(num_series, hidden, lag)
+        self.Linear_Trend = nn.Conv1d(num_series, hidden, lag)
+        self.Linear_Decoder = nn.Conv1d(hidden, 1, 1)
+        
 
     def forward(self, x):
-        # x: [Batch, Input length, Channel]
-        seasonal_init, trend_init = self.decompsition(x)
-        seasonal_init, trend_init = seasonal_init.permute(0,2,1), trend_init.permute(0,2,1)
-        if self.individual:
-            seasonal_output = torch.zeros([seasonal_init.size(0),seasonal_init.size(1),self.pred_len],dtype=seasonal_init.dtype).to(seasonal_init.device)
-            trend_output = torch.zeros([trend_init.size(0),trend_init.size(1),self.pred_len],dtype=trend_init.dtype).to(trend_init.device)
-            for i in range(self.channels):
-                seasonal_output[:,i,:] = self.Linear_Seasonal[i](seasonal_init[:,i,:])
-                trend_output[:,i,:] = self.Linear_Trend[i](trend_init[:,i,:])
-        else:
-            seasonal_output = self.Linear_Seasonal(seasonal_init)
-            trend_output = self.Linear_Trend(trend_init)
+         X = X.transpose(2, 1)
+        
+        seasonal_init, trend_init = self.decompsition(X)
+        seasonal_output = self.Linear_Seasonal(seasonal_init)
+        trend_output = self.Linear_Trend(trend_init)
 
-        x = seasonal_output + trend_output
-        return x.permute(0,2,1) # to [Batch, Output length, Channel]
+        X = seasonal_output + trend_output
+        X = self.Linear_Decoder(X)
+
+        return X.transpose(2, 1)
+
+class cDLinear(nn.Module):
+    def __init__(self, num_series, hidden, lag, kernel_size = 25):
+        super(cDLinear, self).__init__()
+
+        # Decompsition Kernel Size
+        self.decompsition = series_decomp(kernel_size)
+
+        self.p = num_series
+        self.lag = lag
+
+        # Set up networks.
+        self.networks = nn.ModuleList([
+            DLinear(num_series, hidden, lag, kernel_size)
+            for _ in range(num_series)])
+        
+
+    def forward(self, x):
+
+        return torch.cat([network(X) for network in self.networks], dim=2)
+        
