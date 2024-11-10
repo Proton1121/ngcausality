224ca92ddfb17d6805d211c67337b0c8e705584f
Proton1121 <71938858+Proton1121@users.noreply.github.com>
2024-11-10 17:37:54 +0100

    Update cdlinear.py

--- models/cdlinear.py
+++ models/cdlinear.py
@@ -82,3 +82,165 @@ class cDLinear(nn.Module):
 
         return torch.cat([network(X) for network in self.networks], dim=2)
         
+def GC(self, threshold=True, ignore_lag=True):
+        '''
+        Extract learned Granger causality.
+
+        Args:
+          threshold: return norm of weights, or whether norm is nonzero.
+          ignore_lag: if true, calculate norm of weights jointly for all lags.
+
+        Returns:
+          GC: (p x p) or (p x p x lag) matrix. In first case, entry (i, j)
+            indicates whether variable j is Granger causal of variable i. In
+            second case, entry (i, j, k) indicates whether it's Granger causal
+            at lag k.
+        '''
+        if ignore_lag:
+            GC = [torch.norm(torch.cat([net.Linear_Seasonal.weight, net.Linear_Trend.weight], dim=0), dim=(0, 2)) 
+               for net in self.networks]
+        else:
+            GC = [torch.norm(torch.cat([net.Linear_Seasonal.weight, net.Linear_Trend.weight], dim=0), dim=0) 
+               for net in self.networks]
+        GC = torch.stack(GC)
+        if threshold:
+            return (GC > 0).int()
+        else:
+            return GC
+
+
+def prox_update(network, lam, lr, penalty):
+    '''
+    Perform in place proximal update on first layer weight matrix.
+
+    Args:
+      network: MLP network.
+      lam: regularization parameter.
+      lr: learning rate.
+      penalty: one of GL (group lasso), GSGL (group sparse group lasso),
+        H (hierarchical).
+    '''
+    W = network.layers[0].weight
+    hidden, p, lag = W.shape
+    if penalty == 'GL':
+        norm = torch.norm(W, dim=(0, 2), keepdim=True)
+        W.data = ((W / torch.clamp(norm, min=(lr * lam)))
+                  * torch.clamp(norm - (lr * lam), min=0.0))
+    elif penalty == 'GSGL':
+        norm = torch.norm(W, dim=0, keepdim=True)
+        W.data = ((W / torch.clamp(norm, min=(lr * lam)))
+                  * torch.clamp(norm - (lr * lam), min=0.0))
+        norm = torch.norm(W, dim=(0, 2), keepdim=True)
+        W.data = ((W / torch.clamp(norm, min=(lr * lam)))
+                  * torch.clamp(norm - (lr * lam), min=0.0))
+    elif penalty == 'H':
+        # Lowest indices along third axis touch most lagged values.
+        for i in range(lag):
+            norm = torch.norm(W[:, :, :(i + 1)], dim=(0, 2), keepdim=True)
+            W.data[:, :, :(i+1)] = (
+                (W.data[:, :, :(i+1)] / torch.clamp(norm, min=(lr * lam)))
+                * torch.clamp(norm - (lr * lam), min=0.0))
+    else:
+        raise ValueError('unsupported penalty: %s' % penalty)
+
+
+def regularize(network, lam, penalty):
+    '''
+    Calculate regularization term for first layer weight matrix.
+
+    Args:
+      network: MLP network.
+      penalty: one of GL (group lasso), GSGL (group sparse group lasso),
+        H (hierarchical).
+    '''
+    W = network.layers[0].weight
+    hidden, p, lag = W.shape
+    if penalty == 'GL':
+        return lam * torch.sum(torch.norm(W, dim=(0, 2)))
+    elif penalty == 'GSGL':
+        return lam * (torch.sum(torch.norm(W, dim=(0, 2)))
+                      + torch.sum(torch.norm(W, dim=0)))
+    elif penalty == 'H':
+        # Lowest indices along third axis touch most lagged values.
+        return lam * sum([torch.sum(torch.norm(W[:, :, :(i+1)], dim=(0, 2)))
+                          for i in range(lag)])
+    else:
+        raise ValueError('unsupported penalty: %s' % penalty)
+
+
+def ridge_regularize(network, lam):
+    '''Apply ridge penalty at all subsequent layers.'''
+    return lam * sum([torch.sum(fc.weight ** 2) for fc in network.layers[1:]])
+
+
+def restore_parameters(model, best_model):
+    '''Move parameter values from best_model to model.'''
+    for params, best_params in zip(model.parameters(), best_model.parameters()):
+        params.data = best_params
+
+def train_model_ista(cmlp, X, lr, max_iter, lam=0, lam_ridge=0, penalty='H',
+                     lookback=5, check_every=100, verbose=1):
+    '''Train model with Adam.'''
+    lag = cmlp.lag
+    p = X.shape[-1]
+    loss_fn = nn.MSELoss(reduction='mean')
+    train_loss_list = []
+
+    # For early stopping.
+    best_it = None
+    best_loss = np.inf
+    best_model = None
+
+    # Calculate smooth error.
+    loss = sum([loss_fn(cmlp.networks[i](X[:, :-1]), X[:, lag:, i:i+1])
+                for i in range(p)])
+    ridge = sum([ridge_regularize(net, lam_ridge) for net in cmlp.networks])
+    smooth = loss + ridge
+
+    for it in range(max_iter):
+        # Take gradient step.
+        smooth.backward()
+        for param in cmlp.parameters():
+            param.data = param - lr * param.grad
+
+        # Take prox step.
+        if lam > 0:
+            for net in cmlp.networks:
+                prox_update(net, lam, lr, penalty)
+
+        cmlp.zero_grad()
+
+        # Calculate loss for next iteration.
+        loss = sum([loss_fn(cmlp.networks[i](X[:, :-1]), X[:, lag:, i:i+1])
+                    for i in range(p)])
+        ridge = sum([ridge_regularize(net, lam_ridge) for net in cmlp.networks])
+        smooth = loss + ridge
+
+        # Check progress.
+        if (it + 1) % check_every == 0:
+            # Add nonsmooth penalty.
+            nonsmooth = sum([regularize(net, lam, penalty)
+                             for net in cmlp.networks])
+            mean_loss = (smooth + nonsmooth) / p
+            train_loss_list.append(mean_loss.detach())
+
+            if verbose > 0:
+                print(('-' * 10 + 'Iter = %d' + '-' * 10) % (it + 1))
+                print('Loss = %f' % mean_loss)
+                print('Variable usage = %.2f%%'
+                      % (100 * torch.mean(cmlp.GC().float())))
+
+            # Check for early stopping.
+            if mean_loss < best_loss:
+                best_loss = mean_loss
+                best_it = it
+                best_model = deepcopy(cmlp)
+            elif (it - best_it) == lookback * check_every:
+                if verbose:
+                    print('Stopping early')
+                break
+
+    # Restore best model.
+    restore_parameters(cmlp, best_model)
+
+    return train_loss_list
