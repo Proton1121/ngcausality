3fd960146707eb21ff54a160cd023bf612ddb24c
Proton1121 <71938858+Proton1121@users.noreply.github.com>
2024-11-17 00:31:51 +0100

    Update cdlinear.py

--- models/cdlinear.py
+++ models/cdlinear.py
@@ -230,6 +230,9 @@ def train_model_ista(cdlinear, X, lr, max_iter, lam=0, lam_ridge=0, penalty='H',
         ridge = sum([ridge_regularize(net, lam_ridge) for net in cdlinear.networks])
         smooth = loss + ridge
 
+        print('%f' % loss)
+        print('%f' % ridge)
+        
         # Check progress.
         if (it + 1) % check_every == 0:
             # Add nonsmooth penalty.
