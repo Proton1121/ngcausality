dac7930a850427bc010615399e8c01f2e1c5b494
Proton1121 <71938858+Proton1121@users.noreply.github.com>
2024-11-19 01:03:44 +0100

    Update cmamba.py

--- models/cmamba.py
+++ models/cmamba.py
@@ -373,6 +373,26 @@ class RMSNorm(nn.Module):
         else:
             return output
 
+def arrange_input(data, context):
+    '''
+    Arrange a single time series into overlapping short sequences.
+
+    Args:
+      data: time series of shape (T, dim).
+      context: length of short sequences.
+    '''
+    assert context >= 1 and isinstance(context, int)
+    input = torch.zeros(len(data) - context, context, data.shape[1],
+                        dtype=torch.float32, device=data.device)
+    target = torch.zeros(len(data) - context, context, data.shape[1],
+                         dtype=torch.float32, device=data.device)
+    for i in range(context):
+        start = i
+        end = len(data) - context + i
+        input[:, i, :] = data[start:end]
+        target[:, i, :] = data[start+1:end+1]
+    return input.detach(), target.detach()
+
 class cMamba(nn.Module):
     def __init__(self, config: MambaConfig):
         
